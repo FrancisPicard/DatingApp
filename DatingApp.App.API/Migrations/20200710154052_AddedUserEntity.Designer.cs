871653c2568846003b73353e628698cedd5a6803
FPicard <fpicard@epsilia.com>
2020-07-14 09:30:54 -0400

    End of Section 3

--- DatingApp.App.API/Migrations/20200710154052_AddedUserEntity.Designer.cs
+++ DatingApp.App.API/Migrations/20200710154052_AddedUserEntity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DatingApp.App.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.App.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200710154052_AddedUserEntity")]
+    partial class AddedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
